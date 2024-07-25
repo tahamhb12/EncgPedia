270f9c7e0a3d5c6b910a40c8d2c075a9bf8b4daf
tahamhb12 <tahamhb12@gmail.com>
2024-07-25 05:55:19 +0100

    add api routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,17 @@
 <?php
 
+use App\Http\Controllers\CoursesController;
+use App\Http\Controllers\RequestsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get("/courses",[CoursesController::class,"index"]);
+Route::post("/courses",[CoursesController::class,"store"]);
+
+Route::get("/requests",[RequestsController::class,"index"]);
+Route::post("/requests",[RequestsController::class,"store"]);
+Route::post("/requests/{id}",[RequestsController::class,"destroy"]);
